678da852f12bcda63f401214d0c64e468f3d0c27
quanganh2627 <quanganh2627@gmail.com>
2015-09-01 14:00:29 +0700

    Add Asus-app sets
    Change-Id: Iafee5a4adaaee9fc7328976bcd3e2711888a1bfc

--- prebuilts/Android.mk
+++ prebuilts/Android.mk
@@ -8,11 +8,226 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_REQUIRED_MODULES := libjni_xt9input
+#$(shell mkdir -p $(TARGET_OUT_SHARED_LIBRARIES))
+#$(shell cp $(LOCAL_PATH)/AsusKeyboard/lib/x86/libjni_xt9input.so $(TARGET_OUT_SHARED_LIBRARIES))
+#
+#$(shell mkdir -p $(TARGET_OUT_APPS)/AsusKeyboard/lib/x86)
+#$(shell ln -sf ../../../../lib/libjni_xt9input.so $(TARGET_OUT_APPS)/AsusKeyboard/lib/x86/libjni_xt9input.so)
+#ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_APPS)/AsusKeyboard/lib/x86/libjni_xt9input.so
+include $(BUILD_PREBUILT)
 
-$(shell mkdir -p $(TARGET_OUT_SHARED_LIBRARIES))
-$(shell cp $(LOCAL_PATH)/AsusKeyboard/lib/x86/libjni_xt9input.so $(TARGET_OUT_SHARED_LIBRARIES))
+###############################################################################
+###############################################################################
+include $(CLEAR_VARS)
 
-$(shell mkdir -p $(TARGET_OUT_APPS)/AsusKeyboard/lib/x86)
-$(shell ln -sf ../../../../lib/libjni_xt9input.so $(TARGET_OUT_APPS)/AsusKeyboard/lib/x86/libjni_xt9input.so)
-ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_APPS)/AsusKeyboard/lib/x86/libjni_xt9input.so
+LOCAL_MODULE := AsusGallery
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_PATH:=$(PRODUCT_OUT)/system/app
+LOCAL_REQUIRED_MODULES:= libgif libjni_face_detection libjni_face_effect libjni_filter_show
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := AsusGalleryBrust
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_PATH:=$(PRODUCT_OUT)/system/app
+LOCAL_REQUIRED_MODULES:= libgifencoder libjni_picbest_static libjni_picclear_static
+include $(BUILD_PREBUILT)
+
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := AsusFMService
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := OemTelephonyApp
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := AsusBackup
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := MobileManager
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := PCLinkManager
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := AsusInputDevices
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := AsusCalculator
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := SMMI_TEST
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := AsusFMRadio
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := AsusLauncher
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := AsusKeyboard
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := MobileManagerService
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := 3CToolbox
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := PCLinkBinary
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ASUSBrowser
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := AsusDrawRes
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
+###############################################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := SARManager
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+
 include $(BUILD_PREBUILT)
---
+++ prebuilts/FIXUP.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+# Run this script to remake the Android.mk in this directory after
+# adding/removing .apk files.
+#
+
+echo 'LOCAL_PATH := $(my-dir)'
+echo ''
+
+for i in `find -name '*.apk'`
+do
+  base=$(echo $(basename $i .apk))
+  dir=$(echo $(dirname $i))
+  echo "###############################################################################"
+  echo "include \$(CLEAR_VARS)"
+  echo ""
+  echo "LOCAL_MODULE := $base"
+  echo "LOCAL_MODULE_TAGS := optional"
+  echo "LOCAL_SRC_FILES := \$(LOCAL_MODULE)/\$(LOCAL_MODULE).apk"
+  echo "LOCAL_MODULE_CLASS := APPS"
+  echo "LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)"
+  echo "LOCAL_CERTIFICATE := PRESIGNED"
+  if [ -d "$dir/lib" ]; then
+    deplibs=`find $dir -name '*.so' -exec basename {} .so \; | sed 's/'\\n'/ /g'`
+    echo "LOCAL_REQUIRED_MODULES := \\
+              $deplibs"
+  fi
+  echo ""
+  echo "include \$(BUILD_PREBUILT)"
+done
