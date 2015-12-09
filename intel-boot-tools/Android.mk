fea208fd4b67dfc5355e93919146a23d897ddfa2
quanganh2627 <quanganh2627@gmail.com>
2015-12-09 23:34:14 +0700

    Bringup to M

--- intel-boot-tools/Android.mk
+++ intel-boot-tools/Android.mk
@@ -1,18 +1,22 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := pack.c
+LOCAL_IS_HOST_MODULE := true
+LOCAL_SRC_FILES := $(LOCAL_PATH)/pack.c
 LOCAL_CFLAGS += -I/usr/include/
 LOCAL_MODULE := pack_intel
-
+LOCAL_MODULE_TAGS := optional
+include $(call include-if-build-from-source,Makefile,$(LOCAL_MODULE),$(LOCAL_PATH))
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := unpack.c
+LOCAL_IS_HOST_MODULE := true
+LOCAL_SRC_FILES := $(LOCAL_PATH)/unpack.c
 LOCAL_CFLAGS += -I/usr/include/
 LOCAL_MODULE := unpack_intel
+LOCAL_MODULE_TAGS := optional
+include $(call include-if-build-from-source,Makefile,$(LOCAL_MODULE),$(LOCAL_PATH))
 
 include $(BUILD_HOST_EXECUTABLE)
 $(call dist-for-goals,dist_files,$(LOCAL_BUILT_MODULE))
+include $(call-all-makefiles-under,$(LOCAL_PATH))
