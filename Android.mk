14fd9f54142e878d39f2aa74d4975d3026519c24
quanganh2627 <quanganh2627@gmail.com>
2016-03-03 04:50:43 +0700

    Build kernel-inline
    Change-Id: I7c4c2e7d84f03f2f828ea3332778368e24e9f409

--- Android.mk
+++ Android.mk
@@ -20,7 +20,7 @@
 # to only building on ARM if they include assembly. Individual makefiles
 # are responsible for having their own logic, for fine-grained control.
 
-ifneq ($(filter a500cg,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),a500cg)
 LOCAL_PATH := $(call my-dir)
 
 #include $(CLEAR_VARS)
@@ -73,21 +73,7 @@ LOCAL_PATH := $(call my-dir)
 #
 #include $(BUILD_PREBUILT)
 #
-#LOCAL_ADDITIONAL_DEPENDENCIES :=  device/asus/a500cg/openssl-prebuilt/Android.mk
-
-#include $(call all-named-subdir-makefiles, openssl-prebuilt libmultidisplay libintel-updater power prebuilts intel-boot-tools)
-include $(call first-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
 include $(call all-makefiles-under,$(LOCAL_PATH))
-# Add sepdk driver
--include vendor/intel/tools/PRIVATE/debug_internal_tools/sepdk/src/AndroidSEP.mk
--include linux/modules/debug_tools/vtunedk/src/pax/AndroidPAX.mk
-
-# Add vtunedk: sep3_xx, vtsspp drivers. PAX driver will be used from sepdk.
--include linux/modules/debug_tools/vtunedk/src/AndroidSEP.mk
--include linux/modules/debug_tools/vtunedk/src/vtsspp/AndroidVTSSPP.mk
-
-# Testbox - Only for engineering and userdebug
-ifneq (,$(filter $(TARGET_BUILD_VARIANT),eng userdebug))
--include linux/modules/PRIVATE/testbox/AndroidTB.mk
-endif
+include $(call first-makefiles-under,$(LOCAL_PATH))
 endif
--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,31 +1,46 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
 ## If the kernel source is present, AndroidBoard.mk will perform a kernel build.
 ## otherwise, AndroidBoard.mk will find the kernel binaries in a tarball.
 ifneq ($(wildcard $(KERNEL_SRC_DIR)/Makefile),)
 TARGET_KERNEL_SOURCE_IS_PRESENT ?= true
 endif
+# PRODUCT_OUT and HOST_OUT are now defined after BoardConfig is included.
+# Add early definition here
+PRODUCT_OUT ?= out/target/product/$(TARGET_DEVICE)
+HOST_OUT ?= out/host/$(HOST_OS)-$(HOST_PREBUILT_ARCH)
 
+build_kernel: $(PRODUCT_OUT)/kernel
 .PHONY: build_kernel
 ifeq ($(TARGET_KERNEL_SOURCE_IS_PRESENT),true)
-#Kernel rules (build from source, or from tarball
-#$(call inherit-product, device/asus/a500cg/openssl-prebuilt/Android.mk)
-include device/asus/a500cg/AndroidKernel.mk
-build_kernel: get_kernel_from_source
+	#Kernel rules (build from source, or from tarball
+	#$(call inherit-product, device/asus/a500cg/openssl-prebuilt/Android.mk)
+	build_kernel: get_kernel_from_source
 else
-build_kernel: get_kernel_from_tarball
+	build_kernel: get_kernel_from_tarball
 endif
+include device/asus/a500cg/AndroidKernel.mk
 
-.PHONY: get_kernel_from_tarball
-get_kernel_from_tarball:
-	tar -xv -C $(PRODUCT_OUT) -f $(TARGET_KERNEL_TARBALL)
-#
+#.PHONY: get_kernel_from_tarball
+#get_kernel_from_tarball:
+#	tar -xv -C $(PRODUCT_OUT) -f $(TARGET_KERNEL_TARBALL)
+
 bootimage: $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RAMDISK_TARGET)
 
 $(INSTALLED_KERNEL_TARGET): build_kernel
 $(INSTALLED_RAMDISK_TARGET): build_kernel
-#for Intel icc
-export ANDROID_SYSROOT :=
-export ANDROID_GNU_X86_TOOLCHAIN :=
-export T := $(gettop)
-export ANDROID_SYSROOT := $(T)/prebuilts/ndk/current/platforms/android-21/arch-x86
-export ANDROID_GNU_X86_TOOLCHAIN := $(T)/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9
+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+-include vendor/intel/tools/PRIVATE/debug_internal_tools/sepdk/src/AndroidSEP.mk
+-include linux/modules/debug_tools/vtunedk/src/pax/AndroidPAX.mk
 
+# Add vtunedk: sep3_xx, vtsspp drivers. PAX driver will be used from sepdk.
+-include linux/modules/debug_tools/vtunedk/src/AndroidSEP.mk
+-include linux/modules/debug_tools/vtunedk/src/vtsspp/AndroidVTSSPP.mk
+-include linux/modules/debug_tools/socwatchdk/src/AndroidSOCWatchDK.mk
+-include linux/modules/debug_tools/powerdk/src/AndroidPowerDK.mk
+
+# Testbox - Only for engineering and userdebug
+ifneq (,$(filter $(TARGET_BUILD_VARIANT),eng userdebug))
+-include linux/modules/PRIVATE/testbox/AndroidTB.mk
+endif
